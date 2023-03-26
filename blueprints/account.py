3f066b20ea73338eaf6ce82194ab8a9fafe72dd4
ganiular <ganiular@gmail.com>
2023-03-26 06:30:30 +0100

    second commit

--- blueprints/account.py
+++ blueprints/account.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect, request
+from flask import Blueprint, render_template, session, redirect, request, abort
 import db
 
 bp = Blueprint('account', __name__)
@@ -30,21 +30,40 @@ def index():
     user_id = session.get('user_id')
     if user_id is None:
         return render_template('index.html')
-    
-    if session['account_type'] == 'employer':
-        conn = db.connect()
-        employer = conn.execute(
-            'SELECT * FROM users JOIN employers ON users.id=employers.user_id WHERE users.id=?',
-            [user_id]).fetchone()
-        return render_template('index.html', employer=employer)
-    elif session['account_type'] == 'applicant':
-        conn = db.connect()
-        applicant = conn.execute(
-            'SELECT * FROM users JOIN applicants ON users.id=applicants.user_id '
-            'JOIN qualifications ON applicants.qualification_id=qualifications.id WHERE users.id=?',
-            [user_id]).fetchone()
-        return render_template('index.html', applicant=applicant)
+    else:
+        return redirect('/profile/'+ str(user_id))
 
-@bp.get('/profile')
-def profile():
-    return render_template('profile.html')
+# View any user profile page
+@bp.get('/profile/<int:user_id>')
+def profile(user_id):
+    # know the account type of the user
+    conn = db.connect()
+    user = conn.execute(
+        'SELECT * FROM users WHERE id=?', [user_id]
+    ).fetchone()
+    if user is None:
+        # stop the resquest with page not found
+        abort(404)
+    else:
+        if user['account_type'] == 'applicant':
+            applicant = conn.execute(
+                'SELECT * FROM users JOIN applicants ON users.id=applicants.user_id '
+                'JOIN qualifications ON applicants.qualification_id=qualifications.id WHERE users.id=?',
+                [user_id]).fetchone()
+            return render_template('profile.html', applicant=applicant, user_id=user_id)
+        else:
+            employer = conn.execute(
+                'SELECT * FROM users JOIN employers ON users.id=employers.user_id WHERE users.id=?',
+                [user_id]).fetchone()
+            return render_template('profile.html', employer=employer, user_id=user_id)
+        
+@bp.get('/profile/edit')
+def edit_profile():
+    user_id = session['user_id']
+    account_type = session['account_type']
+    conn = db.connect()
+
+    if(account_type == 'applicant'):
+        applicant = conn.execute(
+            
+        )
